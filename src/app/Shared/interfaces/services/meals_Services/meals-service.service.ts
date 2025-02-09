aae3a00232a2ac6788af2db9725c5e2eaadce148
AlamirSamir <Alamirsamir0@gmail.com>
2025-02-09 22:41:01 +0200

    SecondProject

---
+++ src/app/Shared/interfaces/services/meals_Services/meals-service.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MeetServiceService {
+
+  constructor() { }
+
+ private readonly _httpClient= inject(HttpClient)
+
+ getMeetData():Observable<any>{
+   return this._httpClient.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=`)
+ }
+
+
+
+}
